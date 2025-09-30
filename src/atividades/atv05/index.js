db29d0a60f1c7896a09f2684cfd35389c53f1f85
dev_Julio <j.duque.junior@gmail.com>
2025-09-30 14:54:05 -0300

    update index e style JS

--- src/atividades/atv05/index.js
+++ src/atividades/atv05/index.js
@@ -1,55 +1,116 @@
 import { useState } from 'react';
-import { View, Text, TextInput, Touchable, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
 
 import styles from './styles';
 
 export default function Atividade5() {
+  const [texto, setN1] = useState('');
+  const [texto2, setN2] = useState('');
+  const [total, setTotal] = useState('-');
+  const [sinal, setSinal] = useState('+');
+  const [operacao, setOperacao] = useState(1);
+  //1-Soma, 2-Subtração, 3-Multiplicação, 4-Divisão
 
-    const [texto, setN1] = useState('');
-    const [texto2, setN2] = useState('');
-    const [name, setName] = useState('-');
-
-    function handleExibeMensagem() {
-        setName(`${texto} ${texto2}`);
-        setTexto2('');
-        setTexto('');
+  function funcCalcula() {
+    if (operacao == 1) {
+      setTotal(parseInt(texto) + parseInt(texto2));
     }
+    if (operacao == 2) {
+      setTotal(parseInt(texto) - parseInt(texto2));
+    }
+    if (operacao == 3) {
+      setTotal(parseInt(texto) * parseInt(texto2));
+    }
+    if (operacao == 4) {
+      setTotal(parseInt(texto) / parseInt(texto2));
+    }
+  }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.titulo}>Atividade 05</Text>
+  function funcMais() {
+    setOperacao(1);
+    setSinal('﹢');
+  }
 
-            <Text style={styles.legenda}>1º número:</Text>
-            <TextInput
-                value={texto}
-                onChangeText={setN1}
-                keyboardType='ascii-capable'
-                style={styles.textInput}
-            />
+  function funcMenos() {
+    setOperacao(2);
+    setSinal('-');
+  }
 
-            <Text style={styles.legenda}>+</Text>
+  function funcVezes() {
+    setOperacao(3);
+    setSinal('×');
+  }
 
-            <Text style={styles.legenda}>2º número:</Text>
-            <TextInput
-                value={texto2}
-                onChangeText={setN2}
-                keyboardType='ascii-capable'
-                style={styles.textInput}
-            />
+  function funcDivide() {
+    setOperacao(4);
+    setSinal('÷');
+  }
 
-            <Text style={styles.legenda}>=</Text>
-            <Text style={styles.legenda}>Total:</Text>
+  function funcLimpa() {
+    setN1('');
+    setN2('');
+    setTotal('-');
+    setSinal('+');
+  }
 
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Atividade 05</Text>
 
-            <Text style={styles.textInput}>{name}</Text>
+      <Text style={styles.legenda}>1º número:</Text>
+      <TextInput
+        value={texto}
+        onChangeText={setN1}
+        keyboardType="ascii-capable"
+        style={styles.textInput}
+      />
 
-            <View style={styles.txtOperacoes}>=</View>
+      <Text style={styles.legenda}>{sinal}</Text>
 
-            <TouchableOpacity
-                style={styles.botao}
-                onPress={() => handleExibeMensagem()}>
-                <Text style={styles.txtBotao}>CALCULAR</Text>
-            </TouchableOpacity>
-        </View >
-    );
+      <Text style={styles.legenda}>2º número:</Text>
+      <TextInput
+        value={texto2}
+        onChangeText={setN2}
+        keyboardType="ascii-capable"
+        style={styles.textInput}
+      />
+
+      <Text style={styles.legenda}>=</Text>
+      <Text style={styles.legenda}>Total:</Text>
+
+      <Text style={styles.textInput}>{total}</Text>
+
+      <View style={styles.txtOperacoes}>
+        <TouchableOpacity style={styles.contas} onPress={() => funcMais()}>
+          <Text style={styles.simbolos}>﹢</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.contas} onPress={() => funcMenos()}>
+          <Text style={styles.simbolos}>﹣</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.contas} onPress={() => funcVezes()}>
+          <Text style={styles.simbolos}>×</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.contas} onPress={() => funcDivide()}>
+          <Text style={styles.simbolos}>÷</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.contas} onPress={() => funcLimpa()}>
+          <Text style={styles.simbolos}>⌫</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.botao} onPress={() => funcCalcula()}>
+        <Text style={styles.txtBotao}>CALCULAR</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
